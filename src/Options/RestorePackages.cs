56bb26c3b94c513f79089380be9581e9326012c6
Pouya Vedadiyan <vedadiyan@gmail.com>
2022-06-27 12:29:36 +0800

    Clean

--- src/Options/RestorePackages.cs
+++ src/Options/RestorePackages.cs
@@ -7,6 +7,8 @@ public class RestorePackages
 {
     [Option("update", Required = false, HelpText = "Updates existing package")]
     public bool Update { get; set; }
+    [Option("update-global", Required = false, HelpText = "Updates global package")]
+    public bool UpdateGlobal { get; set; }
     [Option("tidy", Required = false, HelpText = "Runs `go tidy` after restoring packages")]
     public bool Tidy { get; set; }
 }
